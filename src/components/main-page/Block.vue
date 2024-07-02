38052f44b892271895d02a08f74d4ec5da201eb5
Josquin2 <Josquinwork@mail.ru>
2024-07-03 01:29:48 +0300

    added ads

---
+++ src/components/main-page/Block.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  background: String,
+  header: String,
+  paragraph: String,
+  buttonClass: String
+})
+</script>
+
+<template>
+  <div class="one-big-block">
+    <img :src="props.background" alt="" class="background" />
+    <div class="info">
+      <h2>{{ props.header }}</h2>
+      <p>{{ props.paragraph }}</p>
+      <button :class="props.buttonClass">Rental Car</button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.one-big-block {
+  width: 640px;
+  height: 360px;
+
+  .background {
+    position: absolute;
+    z-index: 1;
+  }
+  .info {
+    position: absolute;
+    z-index: 2;
+    background: transparent !important;
+    font-synthesis: none;
+    padding: 24px;
+    h2,
+    p {
+      background: transparent !important;
+      color: #ffffff;
+    }
+
+    h2 {
+      font-size: 32px;
+      font-weight: 400;
+      max-width: 272px;
+    }
+
+    p {
+      margin-top: 16px;
+      max-width: 284px;
+    }
+
+    button {
+      cursor: pointer;
+      margin-top: 20px;
+      width: 120px;
+      height: 44px;
+      border: none;
+      border-radius: 5px;
+      font-weight: 500;
+      font-size: 16px;
+      color: #ffffff;
+    }
+    .dark-blue {
+      background-color: #3563e9;
+    }
+    .light-blue {
+      background-color: #54a6ff;
+    }
+  }
+}
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,12 +1,38 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Block from '@/components/main-page/Block.vue'
+</script>
 
 <template>
-  <div class="main-page">Main page</div>
+  <div class="main-page">
+    <div class="ad-blocks">
+      <Block
+        :background="'/public/img/main-page/ads-1.png'"
+        :header="'The Best Platform for Car Rental'"
+        :paragraph="'Ease of doing a car rental safely and reliably. Of course at a low price.'"
+        :button-class="'dark-blue'"
+      />
+      <Block
+        :background="'/public/img/main-page/ads-2.png'"
+        :header="'Easy way to rent a car at a low price'"
+        :paragraph="'Providing cheap car rental services and safe and comfortable facilities.'"
+        :button-class="'light-blue'"
+      />
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
 .main-page {
+  margin-top: 32px;
   padding: 0 4.44vw;
   min-height: 60vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .ad-blocks {
+    display: flex;
+    gap: 32px;
+  }
 }
 </style>
